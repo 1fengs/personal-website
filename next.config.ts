e76bf750245a8e33a6eafe25f5072b31accf8b74
Yifeng <evanshen95@gmail.com>
2026-03-13 17:42:25 +0100

    change config after using vercel

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  basePath: "/personal-website",
-  assetPrefix: "/personal-website/",
 };
 
 export default nextConfig;
